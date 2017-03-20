84ba776d44f45e93db274858a24be3c733791dcd
j <emailme.jtg+github@gmail.com>
2017-03-19 18:14:36 -0700

    tweak console.lua to work better on initial startup

--- console.lua
+++ console.lua
@@ -1,32 +1,44 @@
 local console = hs.console
 
 local application = require("application")
+local timer = require("timer")
 
 local _alpha = console.alpha
 console.alpha = _alpha()
 local useAlpha = _alpha() < 1
+local hswindow = console.hswindow
 
 local function app()
-  return console.hswindow():application()
+  return hswindow():application()
 end
 
 function console.keyStrokes(text)
   hs.eventtap.keyStrokes(text, app())
 end
 
-hs.openConsole(false)
-application.hotkey(
-  app(),
-  {"cmd"},
-  "u",
-  function(app)
-    useAlpha = not useAlpha
-    if useAlpha then
-      _alpha(console.alpha)
-    else
-      _alpha(1)
+timer.waitUntil(
+  function()
+    return hswindow()
+  end,
+  function()
+    local hotkey, watcher = application.hotkey(
+      app(),
+      {"cmd"},
+      "u",
+      function(app)
+        useAlpha = not useAlpha
+        if useAlpha then
+          _alpha(console.alpha)
+        else
+          _alpha(1)
+        end
+      end
+    )
+    if app():isFrontmost() then
+      hotkey:enable()
     end
-  end
+  end,
+  0.1
 )
 
 return console
